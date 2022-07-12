b0867518bbaa7be718b40bd228b459c34f8a81b7
RayPan13 <Ray0928066818@gmail.com>
2022-07-12 13:10:00 +0800

    完成第二段動畫

--- day2/js/index.js
+++ day2/js/index.js
@@ -1,6 +1,20 @@
 'use strict'
 ;(function () {
+    let isClose = false
     document.querySelector('.btn').addEventListener('click', function () {
-        this.classList.toggle('active')
+        if (isClose) {
+            document.querySelector('.top').classList.toggle('rotate')
+            document.querySelector('.bottom').classList.toggle('rotate')
+            setTimeout(() => {
+                this.classList.toggle('active')
+            }, 500)
+        } else {
+            this.classList.toggle('active')
+            setTimeout(() => {
+                document.querySelector('.top').classList.toggle('rotate')
+                document.querySelector('.bottom').classList.toggle('rotate')
+            }, 500)
+        }
+        isClose = !isClose
     })
 })()
