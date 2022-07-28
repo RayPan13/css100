b2264c3e3455828a54735b678a5be9a1ee275128
RayPan13 <Ray0928066818@gmail.com>
2022-07-28 12:37:55 +0800

    調上傳同步動畫

--- day15/js/index.js
+++ day15/js/index.js
@@ -3,6 +3,8 @@
     const fileName = document.querySelector('.uploadBox span')
     const uploadIcon = document.querySelector('.upload')
     const transitionIcon = document.querySelector('.transition')
+    const process = document.querySelector('.process')
+    let hasFiles = false
     function showFileName(name) {
         uploadIcon.style.display = 'none'
         fileName.style.opacity = 1
@@ -12,9 +14,15 @@
         fileName.style.opacity = 0
         transitionIcon.style.display = 'block'
         transitionIcon.classList.add('rotate')
+        process.style.width = '100%'
     }
     document.querySelector('.uploadBox input').addEventListener('change', function (e) {
+        hasFiles = e.target.files.length >= 1
         showFileName(e.target.files[0].name)
     })
-    document.querySelector('.uploadBtn').addEventListener('click', syncing)
+    document.querySelector('.uploadBtn').addEventListener('click', function () {
+        if (hasFiles) {
+            syncing()
+        }
+    })
 })()
