4d2ce02a496bcd39b230ce11d60420aeb0b72230
RayPan13 <Ray0928066818@gmail.com>
2022-07-28 12:23:25 +0800

    add sync func

--- day15/js/index.js
+++ day15/js/index.js
@@ -1,13 +1,20 @@
 'use strict'
 ;(function () {
     const fileName = document.querySelector('.uploadBox span')
-    const uploadIcon = document.querySelector('.uploadIcon')
+    const uploadIcon = document.querySelector('.upload')
+    const transitionIcon = document.querySelector('.transition')
     function showFileName(name) {
         uploadIcon.style.display = 'none'
         fileName.style.opacity = 1
         fileName.textContent = name
     }
+    function syncing() {
+        fileName.style.opacity = 0
+        transitionIcon.style.display = 'block'
+        transitionIcon.classList.add('rotate')
+    }
     document.querySelector('.uploadBox input').addEventListener('change', function (e) {
         showFileName(e.target.files[0].name)
     })
+    document.querySelector('.uploadBtn').addEventListener('click', syncing)
 })()
