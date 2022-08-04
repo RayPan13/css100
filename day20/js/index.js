5eb765b36f45f0f3ec3f88b579f8484ee5f63c55
RayPan13 <Ray0928066818@gmail.com>
2022-08-04 12:40:34 +0800

    新增圓圈動畫

--- day20/js/index.js
+++ day20/js/index.js
@@ -1,27 +1,30 @@
 'use strict'
 ;(function () {
     let btn = document.querySelector('.btn')
+    let circle = document.querySelector('.circle')
     let mail = document.querySelector('.mail')
     let plane = document.querySelector('.plane')
     function setButtonAnimation() {
         if (btn.classList.contains('reset')) {
             btn.style = ''
             btn.classList.remove('reset')
+            circle.classList.remove('send')
             mail.classList.remove('send')
             plane.classList.remove('send')
             btn.textContent = 'SEND MAIL'
         } else {
             btn.classList.add('send')
             setMailAnimation()
+            setCircleAnimation()
             setTimeout(() => {
                 btn.classList.remove('send')
                 btn.classList.add('reset')
-                btn.style.transform = 'scale(0)'
+                btn.style.transform = 'translateX(-50%) scale(0)'
                 btn.style.opacity = '0'
                 btn.textContent = 'RESET'
             }, 3200)
             setTimeout(() => {
-                btn.style.transform = 'scale(1)'
+                btn.style.transform = 'translateX(-50%) scale(1)'
                 btn.style.opacity = '1'
             }, 3700)
         }
@@ -29,6 +32,9 @@
     function setMailAnimation() {
         mail.classList.add('send')
         plane.classList.add('send')
+    }
+    function setCircleAnimation() {
+        circle.classList.add('send')
     }
     btn.addEventListener('click', setButtonAnimation)
 })()
