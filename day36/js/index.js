c77dca56ee7144c543a101899613f1eb86d8d687
RayPan13 <Ray0928066818@gmail.com>
2022-09-13 15:55:39 +0800

    add animation

--- day36/js/index.js
+++ day36/js/index.js
@@ -1,13 +1,26 @@
 'use strict';
 ;(function() {
     const tabs = document.querySelectorAll('.tab')
+    const content = document.querySelector('.content')
+    const textMapping = ['Dashboard','Comments','Notifications','Settings']
+    let animation = false
     function clickTab(target){
+        animation = true
         tabs.forEach(el => el.classList.remove('active'))
+        content.classList.remove('active')
         target.classList.add('active')
+        content.classList.add('active')
+        setTimeout(() => {
+            content.querySelector('h3').textContent = textMapping[target.dataset.idx]
+        }, 500);
+        setTimeout(() => {
+            content.classList.remove('active')
+            animation = false
+        }, 1000);
     }
     tabs.forEach(el => {
         el.addEventListener('click',function(e){
-            clickTab(e.target)
+            animation ? '': clickTab(e.target)
         })
     })
 })()
