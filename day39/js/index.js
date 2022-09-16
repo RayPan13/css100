0222912e7f5679a90bee5d636e6ef526fbbf1264
RayPan13 <Ray0928066818@gmail.com>
2022-09-16 12:49:09 +0800

    add icon remove animation control

--- day39/js/index.js
+++ day39/js/index.js
@@ -1,8 +1,16 @@
-'use strict';
-;(function() {
+'use strict'
+;(function () {
     const icon = document.querySelector('.icon')
-    icon.addEventListener('click',function(){
+    const menu = document.querySelectorAll('.menu a')
+    icon.addEventListener('click', function () {
         icon.classList.add('open')
         icon.classList.remove('close')
+    })
+    menu.forEach((el) => {
+        el.addEventListener('click', function (e) {
+            e.preventDefault()
+            icon.classList.add('close')
+            icon.classList.remove('open')
+        })
     })
 })()
