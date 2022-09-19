a3fe941bec8b1111b1b8dc1e2a87fa0179f914d7
RayPan13 <Ray0928066818@gmail.com>
2022-09-19 11:46:51 +0800

    add active animation

--- day40/js/index.js
+++ day40/js/index.js
@@ -3,12 +3,16 @@
     let pics = document.querySelectorAll('.pic')
     pics.forEach((el, index) => {
         el.addEventListener('click', function () {
-            setAway()
+            setAway(index)
         })
     })
-    function setAway() {
-        pics.forEach(pic => {
+    function setAway(idx) {
+        pics.forEach((pic,index) => {
             pic.classList.toggle('away')
+            if(idx === index){
+                pic.classList.remove('away')
+                pic.classList.add('active')
+            }
         })
     }
 })()
